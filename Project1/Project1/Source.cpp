6046efb8b209529fbd4a2658fefc5466c79e6a37
Administrator <Administrator@P2_11>
2020-10-21 11:36:37 +0700

    chi truong

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,1 +1,34 @@
-sddasdasdasd
+#include <iostream>
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+
+	int a = rand() % 101;
+	cout << "\n So thu 1 la :" << a;
+	int b = rand() % 101;
+	cout << "\n So thu 2 la :" << b;
+	int n;
+	cout << "\nMoi ban nhap kq hieu 2 so";
+	cin >> n;
+	if (n == a - b)
+	{
+		cout << "Ban da Nhap dung";
+	}
+	else
+	{
+		cout << "Ban Nhap Sai!" << "\nKq dung la :" << a - b;
+	}
+	cout << "\nMoi ban nhap kq tong 2 so";
+	cin >> n;
+	if (n == a + b)
+	{
+		cout << "Ban da Nhap dung";
+	}
+	else
+	{
+		cout << "Ban Nhap Sai!" << "\nKq dung la :" << a + b;
+	}
+
+	return 0;
+}

37e80055df0df68020001f5baefd288b10763386
Administrator <callmezed.1999@gmail.com>
2020-10-21 11:24:24 +0700

    danghienchi

--- source.cpp
+++ source.cpp
@@ -1,21 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
 int USCLN(int a, int b)
 {
 
 }
 void hello()
 {
-	
-	cout<<"thay bang";
+
+	cout << "thay bang";
 }
 int tru(int e, int d)
 {
-	cout << "e tru d"<<"="<<a - b;
+	cout << "e tru d" << "=" << a - b;
 }
 int cong(int w, int b)
 {
-	cout << "w cong b" <<"="<< a + b;
+	cout << "w cong b" << "=" << a + b;
 }
 int nhan(int a, int b)
 {
-	cout << "a nhan b "<<"="<< a * b;
+	cout << "a nhan b " << "=" << a * b;
+}
+
+void hieu()
+{
+	int nhap;
+	srand(time(NULL));
+	int a = rand() % (100 - 0 + 1) + 0;
+	int b = rand() % (100 - 0 + 1) + 0;
+	cout << a " - " << b << " = " << ;
+	cin >> nhap;
+	if (nhap == a - b)
+		cout << "dung";
+	else
+		cout << "sai";
+
 }
